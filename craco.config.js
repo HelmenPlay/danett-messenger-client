df72a8976bf0c87c00ccede00fb23fd440662734
HelmenPlay <helmenplay888@gmail.com>
2026-02-24 21:32:40 +0300

    Update craco.config.js

--- craco.config.js
+++ craco.config.js
@@ -1,20 +1,12 @@
-const webpack = require('webpack');
-
 module.exports = {
   webpack: {
     configure: {
       resolve: {
         fallback: {
           buffer: require.resolve('buffer'),
-          process: require.resolve('process/browser'),
-        },
-      },
-      plugins: [
-        new webpack.ProvidePlugin({
-          Buffer: ['buffer', 'Buffer'],
-          process: 'process/browser',
-        }),
-      ],
-    },
-  },
+          process: require.resolve('process/browser')
+        }
+      }
+    }
+  }
 };
